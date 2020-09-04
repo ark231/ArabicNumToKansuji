c5616b5b95a5a73eabffbbe27cc45a402b249747
ark314 <arklp20909@gmai.com>
2020-09-05 00:17:00 +0900

    fix zeros probrem

--- conv-suji.py
+++ conv-suji.py
@@ -9,12 +9,24 @@ kansuji_small={
 
     }
 """
+kansuji_figure=(
+    ("〇"      ,0),
+    ("一"      ,1),
+    ("二"      ,2),\
+    ("三"      ,3),\
+    ("四"      ,4),\
+    ("五"      ,5),\
+    ("六"      ,6),\
+    ("七"      ,7),\
+    ("八"      ,8),\
+    ("九"      ,9),\
+)
 kansuji_kurai=(
     (""        ,1),
     ("十"      ,10),\
     ("百"      ,100),\
     ("千"      ,1000),\
-    )
+)
 kansuji_big=(
     (""        ,1),\
     ("万"      ,10000),\
@@ -34,27 +46,30 @@ kansuji_big=(
     ("那由他"  ,1000000000000000000000000000000000000000000000000000000000000),\
     ("不可思議",10000000000000000000000000000000000000000000000000000000000000000),\
     ("無量大数",100000000000000000000000000000000000000000000000000000000000000000000)\
-    )
+)
 argv=sys.argv
 argc=len(argv)
 #print("argv:{argv}\nargc:{argc}\nargv[argc-1]:{argv1}".format(argv=argv,argc=argc,argv1=argv[argc-1]))
 #----- options -----
-options={"mixed":False,"debug":False}
+options={"mixed":False,"debug":False,"all":False}
 if "--mixed" in argv or "-m" in argv:
     options["mixed"]=True
 if "--stdin" in argv:
     intmp=sys.stdin.read()
 else:
     intmp=argv[argc-1]
+if "--all-kansuji" in argv or "-a" in argv:
+    options["all"]=True
 if "--debug" in argv:
     print("argv:{argv}\nargc:{argc}\nargv[argc-1]:{argv1}".format(argv=argv,argc=argc,argv1=argv[argc-1]),file=sys.stderr)
     options["debug"]=True
 if "-h" in argv:
     print("conv-suji [options...] [number]")
-    print("-m, --mixed   :漢数字と算用数字の折衷表記　例　1234万5678")
-    print("--stdin       :パイプで送られてきた入力を処理する")
-    print("--debug       :デバッグ表示をオンにする")
-    print("-h, --help    :このヘルプを表示して終了する")
+    print("-m, --mixed       :漢数字と算用数字の折衷表記　例　1234万5678")
+    print("-a, --all-kansuji :完全な漢数字表記　例　千百三十四万五千六百七十八")
+    print("--stdin           :パイプで送られてきた入力を処理する")
+    print("--debug           :デバッグ表示をオンにする")
+    print("-h, --help        :このヘルプを表示して終了する")
     sys.exit(0)
 
 checked=re.search('[^\d.]',intmp)
@@ -64,7 +79,7 @@ if options["debug"] == True:
     print(checked,file=sys.stderr) 
 
 if checked != None and checked.group(0) != '\n':
-    print("数の入力には数字と小数点しか使えません。")
+    print("数の入力には数字と小数点しか使えません。",file=sys.stderr)
     sys.exit(1)
 if re.search('\.',intmp) == None: 
     bigger_than_1=True
@@ -73,7 +88,11 @@ else:
 result_tmp=""
 kurai=0
 kurai_bigger=0
+num_zeros=0
 for cnow in reversed(intmp):
+    #print(num_zeros)
+    if cnow == "\n":
+        continue
     if bigger_than_1==False and cnow != '.':
         result_tmp+=cnow
     elif bigger_than_1==False and cnow == '.':
@@ -82,24 +101,42 @@ for cnow in reversed(intmp):
     elif bigger_than_1==True:
         if kurai_bigger<=len(kansuji_big):
             if kurai%4==0:
+                if num_zeros == 4:
+                    result_tmp=result_tmp.replace(kansuji_big[kurai_bigger-1][0],'')
+                    if options["mixed"] == True:
+                        result_tmp=result_tmp[:len(result_tmp)-4]
+                num_zeros=0
                 if cnow == '0':
-                    result_tmp+=(kansuji_big[kurai_bigger][0])
+                    if options["all"] == True:
+                        result_tmp+=(kansuji_big[kurai_bigger][0])
+                    else:
+                        result_tmp+=(kansuji_big[kurai_bigger][0]+cnow)
+                    num_zeros+=1
                 elif cnow == '1':
-                    result_tmp+=(kansuji_big[kurai_bigger][0])
+                    if options["all"] == True:
+                        cnow=kansuji_figure[int(cnow)][0]
+                    result_tmp+=(kansuji_big[kurai_bigger][0]+cnow)
                 else:
+                    if options["all"] == True:
+                        cnow=kansuji_figure[int(cnow)][0]
                     result_tmp+=(kansuji_big[kurai_bigger][0]+cnow)
                 kurai+=1
                 kurai_bigger+=1
-            elif options["mixed"] == False:
-                if cnow == '0':
-                    result_tmp+=''
-                elif cnow == '1':
-                    result_tmp+=(kansuji_kurai[kurai%4][0])
-                else:
-                    result_tmp+=(kansuji_kurai[kurai%4][0]+cnow)
-                kurai+=1
             else:
-                result_tmp+=cnow
+                if options["mixed"] == False or options["all"] == True:
+                    if cnow == '0':
+                        num_zeros+=1
+                        #(1+1==3)==True
+                    elif cnow == '1':
+                        result_tmp+=(kansuji_kurai[kurai%4][0])
+                    else:
+                        cnow=kansuji_figure[int(cnow)][0]
+                        result_tmp+=(kansuji_kurai[kurai%4][0]+cnow)
+                elif cnow == '0':
+                    result_tmp+=cnow
+                    num_zeros+=1
+                else:
+                    result_tmp+=cnow
                 kurai+=1
         else:
             result_tmp+=cnow
